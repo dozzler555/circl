6aa4593d0adeb377cbec8b7a35bdb6a0cc734708
dozzler555 <80205889+dozzler555@users.noreply.github.com>
2021-07-14 22:00:05 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,26 +13,26 @@ docker container prune --force || true
              
 sudo apt install -y python3.8
              
-sudo apt install -y python3-venv
 
-sudo -s
+
+
              
-python3 -m venv venv
+
              
-source venv/bin/activate
+
              
 pip install -r requirements.txt
             
-apt install -y postgresql postgresql-contrib
-apt -qq install -y curl git wget python3 python3-pip aria2 ffmpeg mediainfo unzip p7zip-full p7zip-rar
+sudo apt install -y postgresql postgresql-contrib
+sudo apt -qq install -y curl git wget python3 python3-pip aria2 ffmpeg mediainfo unzip p7zip-full p7zip-rar
              
-curl https://rclone.org/install.sh | bash
-apt-get install -y software-properties-common
+curl https://rclone.org/install.sh | sudo bash
+sudo apt-get install -y software-properties-common
              
-apt-get -y update
+sudo apt-get -y update
              
-add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable
+sudo add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable
              
-apt install -y qbittorrent-nox
+sudo apt install -y qbittorrent-nox
              
 export PORT=8080
