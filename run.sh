1446c8828a6996307a5b7a5e1e31b63e46686d1c
dozzler555 <80205889+dozzler555@users.noreply.github.com>
2021-07-14 22:04:33 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -21,7 +21,7 @@ sudo apt install -y python3.8
              
 
              
-pip install -r requirements.txt
+pip3.8 install -r requirements.txt
             
 sudo apt install -y postgresql postgresql-contrib
 sudo apt -qq install -y curl git wget python3 python3-pip aria2 ffmpeg mediainfo unzip p7zip-full p7zip-rar
